eaedeb08960c8f3d3583be31662107b5dcbb3e38
AlexMFV <alexmfv1@gmail.com>
2018-12-06 08:46:39 +0000

    Tests with bombs

--- Minesweeper/main.py
+++ Minesweeper/main.py
@@ -1,5 +1,17 @@
+from random import *
 from graphics import *
 
 def main():
-    win = GraphWin("Minesweeper", )
+    win = GraphWin("Minesweeper", 160, 210)
+    matrix = [][]
+    addBombs(matrix)
+    
+def addBombs(matrix):
+    for i in range(10):
+        x = randint(0, 9)-1
+        y = randint(0, 9)-1
+        matrix[x][y] = "b"
+    
+main()
+    
     
