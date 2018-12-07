08f1f01a5d9a6a06d2afdfa114b35757077407b3
AlexMFV <alexmfv1@gmail.com>
2018-12-07 18:57:09 +0000

    Added more Assets, game is playable but missing features

--- Minesweeper/main.py
+++ Minesweeper/main.py
@@ -1,47 +1,117 @@
 from random import *
 from graphics import *
+import time
 
 def main():
-    win = GraphWin("Minesweeper", 500, 500) #default: 160, 210
     
     #Variables
     matrix = []
-    h = 30     #!
-    w = 30      #!   Needs Level Set
-    bombs = 300  #!
-    numObjs = []
-        
+    h = 9
+    w = 9
+    bombs = 10
+    plates = [] # Buttons on top of the numbers
+    isPlaying = True
+    
+    win = drawWindow(w, h)
+    win.update()
+    
+    #Start loading time...
+    time_start = time.perf_counter()
+    
     #Methods
     initBoard(matrix, w, h)
     addBombs(matrix, bombs, w, h)
-    addNumbers(matrix, w, h)
+    #addNumbers(matrix, w, h)
+    #drawBoardNumbers(win, matrix, w, h)
+    drawCoverPlates(win, matrix, w, h, plates)
     
-    drawBoardNumbers(win, matrix, w, h, numObjs)
+    win.update()
+    
+    #End loading time
+    time_end = time.perf_counter() - time_start
+    print("Time to load:",time_end)
+    
+    #Main Game
+    while isPlaying:
+        click = win.checkMouse()
+        clickPos = processClick(click)
         
+        if clickPos != None:
+            isPlaying = checkNumber(win, matrix, int(clickPos.getX()), int(clickPos.getY()), w, h)
+            undrawCover(clickPos, plates, w)
+            
+        win.update()
         
-
+    showAllBombs(win, matrix, plates, w)
+    
 ##  All methods below this  ##
 
-def drawBoardNumbers(win, matrix, w, h, numObjs):
+'''When the player loses the game all the bombs are revealed'''
+def showAllBombs(win, matrix, plates, w):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            drawNumbers(win, matrix[i][j], i, j, numObjs)
-            
-def drawNumbers(win, num, i, j, numObjs):
-    if num != " ":
-        numObjs.append(drawNumber(win, num, i, j))
-
-def drawNumber(win, num, i, j):
-    pic = "../Resources/" + num + ".png"
-    picture = Image(Point(i*16+8, j*16+8), pic)
-    picture.draw(win)
+            if matrix[i][j] == "b":
+                plates[int(j + w*i)].undraw()
+                drawBomb(win, i, j)
     
-    return picture
+'''Undraws the clicked button'''
+def undrawCover(pos, plates, w):
+    x = pos.getX()
+    y = pos.getY()
+    plates[int(x + (w * y))].undraw()
 
+'''Transforms the clicked position to a grid position'''
+def processClick(click):
+    if click != None:
+        x = click.getX()
+        y = click.getY()
+        x = int(x / 16)
+        y = int(y / 16)
+        return Point(x, y)
+    return None
+
+'''Draws the window, dynamic to the number of squares'''
+def drawWindow(w, h):
+    win = GraphWin("Minesweeper", w*16, h*16, autoflush=False) ## autoflush=False
+    return win
+
+'''Draws all the cover buttons of the grid'''
+def drawCoverPlates(win, matrix, w, h, plates):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            plates.append(drawPlate(win, matrix, i, j))
+            
+'''Draws the cover button in the position retrieved'''
+def drawPlate(win, matrix, i, j):
+    pic = "../Resources/block.gif"
+    cover = Image(Point(j*16+8, i*16+8), pic)
+    cover.draw(win)
+    return cover
+
+# def drawBoardNumbers(win, matrix, w, h):
+#     for i in range(len(matrix)):
+#         for j in range(len(matrix[i])):
+#             if matrix[i][j] != " ":
+#                 drawNumber(win, matrix[i][j], i, j)
+                
+'''Draws the remaining bombs after the player lost the game'''
+def drawBomb(win, i, j):
+    pic = "../Resources/b.gif"
+    pict = Image(Point(j*16+8, i*16+8), pic)
+    pict.draw(win)
+
+'''Draws the number on the screen (also applies for bombs)'''
+def drawNumber(win, num, i, j):
+    pic = "../Resources/" + num + ".gif"
+    picture = Image(Point(j*16+8, i*16+8), pic)
+    picture.draw(win)
+
+'''Initializes the board array with empty values'''
 def initBoard(matrix, w, h):
     for i in range(h):
         matrix.append([" "] * w)
 
+'''Adds the bombs to random place in the board array'''
 def addBombs(matrix, bombs, w, h):
     for i in range(bombs):
         y = randint(0, h)-1
@@ -50,82 +120,71 @@ def addBombs(matrix, bombs, w, h):
             y = randint(0, h)-1
             x = randint(0, w)-1
         matrix[y][x] = "b"
+
+'''Gets and draws the number in the position of the click'''
+def checkNumber(win, matrix, x, y, w, h):
+    if matrix[y][x] == " " or matrix[y][x] == "b":
+        matrix[y][x] = checkAround(matrix, y, x, w, h)
+        drawNumber(win, matrix[y][x], y, x)
         
-def addNumbers(matrix, w, h):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            if matrix[i][j] == " ":
-                matrix[i][j] = checkAround(matrix, i, j, w, h)
+        if matrix[y][x] == "exp":
+            return False
+        else:
+            return True
             
-def checkAround(matrix, y, x, w, h):
+    return True
+        
+# def addNumbers(matrix, w, h):
+#     for i in range(len(matrix)):
+#         for j in range(len(matrix[i])):
+#             if matrix[i][j] == " ":
+#                 matrix[i][j] = checkAround(matrix, i, j, w, h)
+            
+'''Checks around the clicked position for bombs (Gets values for Check())'''
+def checkAround(matrix, i, j, w, h):
     bombsFound = 0
     w = w-1
     h = h-1
     
-    if x > 0 and x < w:
-        if y == 0:
-            bombsFound += topCheck(matrix, y, x)
-        elif y == h:
-            bombsFound += bottomCheck(matrix, y, x)
+    #If its a bomb, explode
+    if matrix[i][j] == "b":
+        return "exp"
+    
+    #Checks the position of the clicked button
+    if j > 0 and j < w:
+        if i == 0:
+            bombsFound += Check(matrix, i, j, 0, 2, -1, 2) #Top check
+        elif i == h:
+            bombsFound += Check(matrix, i, j, -1, 1, -1, 2) #Bottom Check
         else:
-            bombsFound += fullCheck(matrix, y, x)
+            bombsFound += Check(matrix, i, j, -1, 2, -1, 2) #Full Check
     else:
-        if y == 0:
-            if x == 0:
-                bombsFound += topLeftCheck(matrix, y, x)
+        if i == 0:
+            if j == 0:
+                bombsFound += Check(matrix, i, j, 0, 2, 0, 2) #Top Left Check
             else:
-                bombsFound += topRightCheck(matrix, y, x)
-        elif y == h:
-            if x == 0:
-                bombsFound += bottomLeftCheck(matrix, y, x)
+                bombsFound += Check(matrix, i, j, 0, 2, -1, 1) #Top Right Check
+        elif i == h:
+            if j == 0:
+                bombsFound += Check(matrix, i, j, -1, 1, 0, 2) #Bottom Left Check
             else:
-                bombsFound += bottomRightCheck(matrix, y, x)
+                bombsFound += Check(matrix, i, j, -1, 1, -1, 1) #Bottom Right Check
         else:
-            if x == 0:
-                bombsFound += leftCheck(matrix, y, x)
+            if j == 0:
+                bombsFound += Check(matrix, i, j, -1, 2, 0, 2) #Left Check
             else:
-                bombsFound += rightCheck(matrix, y, x)
+                bombsFound += Check(matrix, i, j, -1, 2, -1, 1) #Right Check
             
     return str(bombsFound)
-            
-def topCheck(matrix, i, j):
-    return Check(matrix, i, j, 0, 2, -1, 2)
     
-def bottomCheck(matrix, i, j):
-    return Check(matrix, i, j, -1, 1, -1, 2)
-    
-def leftCheck(matrix, i, j):
-    return Check(matrix, i, j, -1, 2, 0, 2)
-    
-def rightCheck(matrix, i, j):
-    return Check(matrix, i, j, -1, 2, -1, 1)
-    
-def topLeftCheck(matrix, i, j):
-    return Check(matrix, i, j, 0, 2, 0, 2)
-    
-def topRightCheck(matrix, i, j):
-    return Check(matrix, i, j, 0, 2, -1, 1)
-    
-def bottomRightCheck(matrix, i, j):
-    return Check(matrix, i, j, -1, 1, -1, 1)
-    
-def bottomLeftCheck(matrix, i, j):
-    return Check(matrix, i, j, -1, 1, 0, 2)
-    
-def fullCheck(matrix, i, j):
-    return Check(matrix, i, j, -1, 2, -1, 2)
-    
+'''Complement of checkAround (Actually checks around)'''
 def Check(matrix, i, j, s1, e1, s2, e2):
-    value = []
+    val = 0
     
     for line in range(s1, e1):
         for row in range(s2, e2):
-            value.append(matrix[int(i+line)][int(j+row)])
-    
-    val = 0
-    for i in range(len(value)):
-        val += value[i].count("b")
-        
+            if matrix[int(i+line)][int(j+row)] == "b":
+                val += 1
     return val
         
 main()
